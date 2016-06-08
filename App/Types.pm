875cfb1bfab71f2a5dc184fd3e8d2c8674964c82
Bennie Robinson <br951w@att.com>
2016-06-08 09:49:51 -0400

    Remove duplicated files

--- App/Types.pm
+++
@@ -1,9 +0,0 @@
-package App::Types
-{
-    use Moose::Util::TypeConstraints;
-
-    subtype 'ISOCODE'
-    => as 'Str'
-    => where { $_ =~ /^[A-Z]{3}$/ }
-    => message { 'It must be a 3 letter currency ISO code' };
-}
